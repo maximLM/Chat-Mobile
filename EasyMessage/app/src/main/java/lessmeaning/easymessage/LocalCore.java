98ce2338d051f56ecc1a902b831a9cd2eee31ab4
EuropePC <EuropePC>
2016-11-04 11:06:53 +0300

    LockalCore, ListView< EditText and Buttin have created

---
+++ EasyMessage/app/src/main/java/lessmeaning/easymessage/LocalCore.java
@@ -0,0 +1,7 @@
+package lessmeaning.easymessage;
+
+/**
+ * Created by 1 on 03.11.2016.
+ */
+public class LocalCore {
+}
--- EasyMessage/app/src/main/java/lessmeaning/easymessage/MainActivity.java
+++ EasyMessage/app/src/main/java/lessmeaning/easymessage/MainActivity.java
@@ -2,12 +2,32 @@ package lessmeaning.easymessage;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    MainActivity activity;
+    Button mButton;
+    ListView mListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        LocalCore localCore = new LocalCore(MainActivity activity);
+        mButton = (Button) findViewById(R.id.button);
+        mListView = (ListView) findViewById(R.id.list_view);
+        mButton.setOnClickListener(this);
     }
+    @Override
+    public void onClick (View v) {
+        if (v.getId() == mButton.getId()) {
+            localCore.addTemp(inf);
+        }
+    }
+
+    public void
 }
