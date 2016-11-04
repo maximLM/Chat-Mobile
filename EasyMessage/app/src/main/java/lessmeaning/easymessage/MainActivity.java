4b1bc4f3d4a70f839cac9017fceab2067449ee9a
maxim_crywood <lesssmeaning@gmail.com>
2016-11-04 17:41:10 +0300

    it's testing

--- EasyMessage/app/src/main/java/lessmeaning/easymessage/MainActivity.java
+++ EasyMessage/app/src/main/java/lessmeaning/easymessage/MainActivity.java
@@ -1,7 +1,15 @@
 package lessmeaning.easymessage;
 
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
