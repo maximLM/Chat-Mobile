02ee8f360faa3b553f4e11ed1c133e499fec0efc
Sofia <cuzyutinasofia@yandex.ru>
2016-11-04 12:23:33 +0300

    DataBase sqlite

---
+++ EasyMessage/app/src/main/java/lessmeaning/easymessage/Row.java
@@ -0,0 +1,15 @@
+package lessmeaning.easymessage;
+
+/**
+ * Created by пользователь on 03.11.2016.
+ */
+
+public class Row {
+    String content;
+    long time;
+
+    public Row(String content, long time) {
+        this.content = content;
+        this.time = time;
+    }
+}
